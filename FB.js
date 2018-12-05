77bdedba8d3fee95e6206ebfbdf3536c442a1ebb
Cy Bae <cybae0804@gmail.com>
2018-12-05 12:34:47 -0800

    Firebase Basic Setup

---
+++ FB.js
@@ -0,0 +1,30 @@
+class FB {
+    constructor() {
+        console.log('Firebase Constructor Called');
+        this.init();
+        this.testConnection()
+    }
+
+    init() {
+        // we know we shouldn't save api keys, but we're lazy 
+        const config = {
+            apiKey: "AIzaSyACHDJLR_de1uWao0_PxgePqdwHhB9M3R4",
+            authDomain: "hackathon2-9e91e.firebaseapp.com",
+            databaseURL: "https://hackathon2-9e91e.firebaseio.com",
+            projectId: "hackathon2-9e91e",
+            storageBucket: "hackathon2-9e91e.appspot.com",
+            messagingSenderId: "951283584290"
+          };
+
+        this.db = firebase.initializeApp(config).database();
+    }
+
+    testConnection() {
+        const test = this.db.ref('/test/');
+        console.log('testing connection...');
+
+        test.once('value', snapshot => {
+            console.log(snapshot.val())
+        })
+    }
+}
