733241f7fa5098d3a701952223482bb0ff509ae5
MichaelCapo23 <123pie23@gmail.com>
2018-12-06 13:05:00 -0800

    fixed the object being passed to erick, sorted the array given to my from james

--- Document.js
+++ Document.js
@@ -1,14 +1,17 @@
 class Document {
     constructor () {
         console.log('document created');
+        // this.artistArr = ["eminem", "eminem", "ariana", "jcole", "paak", "jid", "jid", "jid"];
+        // this.longlat = {longitude : 39.969938,
+        //                  latitude : -75.160278};
         this.attachClickHandlers();
-        // this.attachClickHandlers = this.attachClickHandlers.bind(this);
-        // this.grabUsername = this.grabUsername.bind(this);
-        // this.clearInputField = this.clearInputField.bind(this);
-        // this.spotifyInstance = new Spotify();
+        this.attachClickHandlers = this.attachClickHandlers.bind(this);
+        this.grabUsername = this.grabUsername.bind(this);
+        this.clearInputField = this.clearInputField.bind(this);
+        this.spotifyInstance = new Spotify();
         this.firebaseInstance = new FB();
-        // this.mapsInstance = new Maps();
-        // this.ticketsInstance = new Tickets();
+        this.mapsInstance = new Maps();
+        this.ticketsInstance = new Tickets();
     }
 
     attachClickHandlers() {
