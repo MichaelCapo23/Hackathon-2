7c902938d663c6cf244ac667e3cbf301ebcd288b
MichaelCapo23 <123pie23@gmail.com>
2019-02-14 17:10:58 -0800

    got info for first artist using songkick

--- public/Tickets.js
+++ public/Tickets.js
@@ -1,87 +1,93 @@
 class Tickets {
-  constructor() {
-    this.concertInfo = {};
-  }
-
-  organizeTickets(location, artists) {
-    const newArtists = artists.reduce((accum, current) => {
-      accum[current] ? accum[current]++ : accum[current] = 1; 
-      
-      return accum;
-    }, {});
-    const arr = []; 
-    
-    for (let key in newArtists) {
-      arr.push([key, newArtists[key]]);
+    constructor() {
+        this.concertInfo = {};
     }
-    
-    const a = arr.sort((a, b) => b[1] - a[1]).map(value => value[0]);
 
-    const promise = new Promise((resolve, reject) => {
-      this.getGeoHash(a, location);
+    organizeTickets(location, artists) {
+        const newArtists = artists.reduce((accum, current) => {
+            accum[current] ? accum[current]++ : accum[current] = 1;
 
-    })
-  }
+            return accum;
+        }, {});
+        const arr = [];
 
-  getGeoHash(artists, location) {
-    var hash = Geohash.encode(location.lng, location.lat, [5]);
-    this.callGetDataFromApi(artists, hash)
-  }
+        for (let key in newArtists) {
+            arr.push([key, newArtists[key]]);
+        }
 
-  callGetDataFromApi(artistArr, hash) {
-    for (var i = 0; i < artistArr.length; i++) {
-      this.getDataFromApi(artistArr[i], i, hash);
+        const a = arr.sort((a, b) => b[1] - a[1]).map(value => value[0]);
+
+        const promise = new Promise((resolve, reject) => {
+            this.getGeoHash(a, location);
+        })
     }
-  }
 
-  getDataFromApi(artistName, index, hash) {
-    this.ajaxCallVar = {
-      // url: 'https://app.ticketmaster.com/discovery/v2/events.json?apikey=ihQ5Lmy34lHVnLU8xKTBu75hBUHVyQAa', // mike
-      url: 'https://app.ticketmaster.com/discovery/v2/events.json?apikey=tI1aYe3CCBWTWjMAkQJRhC6TAKtmanEY', // mike
-      // url: 'https://app.ticketmaster.com/discovery/v2/events.json?apikey=kPkBY3I9s8hIRNOlcc1L0KjBQqtwenIA',  // erick
-      type: 'GET',
-      dataType: 'JSON',
-      data: {
-        geoPoint: hash,
-        keyword: artistName,
-        radius: "8000",
-        unit: "miles"
-      }
-    };
-    $.ajax(this.ajaxCallVar)
-      .then(response => this.organizeResponse(artistName, response, index))
-      .fail(err => console.log('Error', err));
-  }
-  ;
-
-  organizeResponse(name, artistInfo, index) {
-    const info = artistInfo["_embedded"].events;
-    const venue = ["_embedded"]["venues"][0];
-
-    for (var i = 0; i < info.length; i++) {
-      var concertObj = {};
-      concertObj.priceRanges = info[i].priceRanges;
-      concertObj.venue = info[i][venue].name;
-      concertObj.latlog = {
-        latitude: [info[i][venue].location.latitude],
-        longitude: [info[i][venue].location.longitude]
-      };
-      concertObj.address = info[i][venue].address.line1;
-      concertObj.city = info[i][venue].city.name;
-      concertObj.id = info[i].id;
-      concertObj.country = info[i][venue].country.name;
-      concertObj.website = info[i].url;
-      concertObj.date = info[i].dates.start.localDate;
-      concertObj.time = info[i].dates.start.localTime;
-
-      if (!this.concertInfo.hasOwnProperty(name)) {
-        this.concertInfo[name] = [];
-      }
-      this.concertInfo[name][i] = concertObj;
+    getGeoHash(artists, location) {
+        var hash = Geohash.encode(location.lng, location.lat, [5]);
+        this.callGetDataFromApi(artists, hash)
     }
-  }
 
-  getConcertInfo() {
-    return this.concertInfo;
-  }
+    callGetDataFromApi(artistArr, hash) {
+        this.getDataFromApi(artistArr[1], 1, hash);
+    }
+
+    getDataFromApi(artistName, index, hash) {
+        debugger;
+        // `https://api.songkick.com/api/3.0/events.json?apikey=G8tZl3JcQGXwDFIm&artist_name=kanye+west`,
+        this.ajaxCallVar = {
+            type: 'GET',
+            url: `https://api.songkick.com/api/3.0/search/artists.json?apikey=G8tZl3JcQGXwDFIm&query=${artistName}`,
+        };
+        $.ajax(this.ajaxCallVar).then((response) => {
+            debugger;
+            this.getEventInfo(response)
+        })
+    }
+
+    getEventInfo(artistInfo) {
+        debugger;
+        let artistID = artistInfo.resultsPage.results.artist[0].id;
+        this.ajaxCallVar = {
+            url: `https://api.songkick.com/api/3.0/artists/${artistID}/gigography.json?apikey=G8tZl3JcQGXwDFIm`,
+            type: 'GET',
+        };
+        $.ajax(this.ajaxCallVar).then(response => {
+            this.organizeResponse(response)
+        })
+    }
+
+
+    organizeResponse(artistInfo) {
+        debugger;
+        console.log("artistInfo: ", artistInfo);
+        const events = artistInfo.resultsPage.results.event
+        // const info = artistInfo["_embedded"].events;
+        // const venue = ["_embedded"]["venues"][0];
+        //
+        // for (var i = 0; i < info.length; i++) {
+        //   var concertObj = {};
+        //   concertObj.priceRanges = info[i].priceRanges;
+        //   concertObj.venue = info[i][venue].name;
+        //   concertObj.latlog = {
+        //     latitude: [info[i][venue].location.latitude],
+        //     longitude: [info[i][venue].location.longitude]
+        //   };
+        //   concertObj.address = info[i][venue].address.line1;
+        //   concertObj.city = info[i][venue].city.name;
+        //   concertObj.id = info[i].id;
+        //   concertObj.country = info[i][venue].country.name;
+        //   concertObj.website = info[i].url;
+        //   concertObj.date = info[i].dates.start.localDate;
+        //   concertObj.time = info[i].dates.start.localTime;
+        //
+        //   if (!this.concertInfo.hasOwnProperty(name)) {
+        //     this.concertInfo[name] = [];
+        //   }
+        //   this.concertInfo[name][i] = concertObj;
+        // }
+    }
+
+    getConcertInfo() {
+        return this.concertInfo;
+    }
 }
